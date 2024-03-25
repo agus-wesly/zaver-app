164843c92ea0ef8275b1e19d86090fd3ec78df4f
aguswesly <agustchannel@gmail.com>
2024-03-25 21:55:13 +0700

    fix : temporary disable refresh function

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -4,18 +4,17 @@ import { useEffect, useState } from 'react'
 
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
-import useRefreshToken from '@/features/auth/use-refresh-token'
+// import useRefreshToken from '@/features/auth/use-refresh-token'
 import { useAuth } from '@/features/auth/use-auth'
 import { Navbar } from '@/layout/main-layout'
 
 function RootLayout() {
   const [isLoadingApp, setIsLoadingApp] = useState(false)
-  const { refresh } = useRefreshToken()
   const user = useAuth((state) => state.user)
   useEffect(() => {
     const verifyRefreshToken = async () => {
       try {
-        await refresh()
+        // await refresh()
       } catch (err) {
         console.error(err)
       } finally {
